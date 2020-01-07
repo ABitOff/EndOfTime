e8fb81d6fdde14b50e0cb2342a1b2ce1e51d2429
Steven Fontaine <sir.fon1103@gmail.com>
2020-01-07 13:39:48 -0600

    initial commit

---
+++ src/main/java/org/abitoff/mc/eot/EndOfTime.java
@@ -0,0 +1,54 @@
+package org.abitoff.mc.eot;
+
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.lang.reflect.Type;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod("eot")
+public class EndOfTime
+{
+	private static final Logger LOGGER = LogManager.getLogger(EndOfTime.class);
+	private static final Map<String, Object> buildData = getBuildData();
+
+	public EndOfTime()
+	{
+		// Register the setup method for modloading
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+		// Register ourselves for server and other game events we are interested in
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	private void setup(final FMLCommonSetupEvent event)
+	{
+		LOGGER.info("Loading " + buildData.getOrDefault("mod-name", "End of Time") + " version "
+				+ buildData.getOrDefault("mod-version", "?"));
+	}
+
+	private static Map<String, Object> getBuildData()
+	{
+		Type mapType = new TypeToken<Map<String, Object>>()
+		{
+		}.getType();
+		try (Reader r = new InputStreamReader(EndOfTime.class.getResourceAsStream("/build-data.json")))
+		{
+			return new Gson().fromJson(r, mapType);
+		} catch (Exception e)
+		{
+			LOGGER.error("Failed to get End of Time build data!", e);
+			throw new RuntimeException(e);
+		}
+	}
+}
---
+++ src/test/java/org/abitoff/mc/eot/EndOfTimeTest.java
@@ -0,0 +1,14 @@
+package org.abitoff.mc.eot;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class EndOfTimeTest
+{
+	@Test
+	public void TempTest()
+	{
+		assertTrue(!false);
+	}
+}
