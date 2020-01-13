84ccc34bed27608e3d76927929d79a9798993d27
Steven Fontaine <sir.fon1103@gmail.com>
2020-01-13 00:53:06 -0600

    get zombie village pieces more reliably

--- src/main/java/org/abitoff/mc/eot/EndOfTime.java
+++ src/main/java/org/abitoff/mc/eot/EndOfTime.java
@@ -1,7 +1,7 @@
 package org.abitoff.mc.eot;
 
 import java.util.List;
-import java.util.Random;
+import java.util.stream.Collectors;
 
 import org.abitoff.mc.eot.Constants;
 import org.abitoff.mc.eot.world.biome.DeepLavaOceanBiome;
@@ -12,7 +12,6 @@ import org.abitoff.mc.eot.world.biome.provider.BiomeProviderTypeEOT;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import com.google.common.collect.ImmutableList;
 import com.mojang.datafixers.util.Pair;
 
 import net.minecraft.util.ResourceLocation;
@@ -53,32 +52,17 @@ public class EndOfTime
 		LOGGER.info("Loading {} version {}", Constants.MOD_NAME, Constants.MOD_VERSION);
 
 		DesertVillagePools.init();
-		List<JigsawPiece> a = JigsawManager.field_214891_a.get(new ResourceLocation("village/desert/town_centers"))
-				.func_214943_b(new Random());
-		JigsawPiece mp1 = null;
-		JigsawPiece mp2 = null;
-		JigsawPiece mp3 = null;
-		for (JigsawPiece p: a)
+		JigsawPattern pattern = JigsawManager.field_214891_a.get(new ResourceLocation("village/desert/town_centers"));
+		List<Pair<JigsawPiece, Integer>> pairList = pattern.field_214952_d.stream().filter(pair ->
 		{
-			assert p instanceof SingleJigsawPiece;
-			SingleJigsawPiece sjp = (SingleJigsawPiece) p;
-			String location = sjp.location.toString();
-			if (location.contains("zombie"))
-			{
-				char lastChar = location.charAt(location.length() - 1);
-				if (lastChar == '1')
-					mp1 = p;
-				else if (lastChar == '2')
-					mp2 = p;
-				else if (lastChar == '3')
-					mp3 = p;
-			}
-		}
-		assert mp1 != null && mp2 != null && mp3 != null;
-		JigsawManager.field_214891_a.register(new JigsawPattern(
-				new ResourceLocation(Constants.MOD_ID, "village/eot/town_centers"), new ResourceLocation("empty"),
-				ImmutableList.of(new Pair<>(mp1, 2), new Pair<>(mp2, 2), new Pair<>(mp3, 1)),
-				JigsawPattern.PlacementBehaviour.RIGID));
+			JigsawPiece piece = pair.getFirst();
+			if (piece instanceof SingleJigsawPiece)
+				return ((SingleJigsawPiece) piece).location.toString().contains("zombie");
+			return false;
+		}).collect(Collectors.toList());
+		JigsawManager.field_214891_a
+				.register(new JigsawPattern(new ResourceLocation(Constants.MOD_ID, "village/eot/town_centers"),
+						new ResourceLocation("empty"), pairList, JigsawPattern.PlacementBehaviour.RIGID));
 	}
 
 	@EventBusSubscriber(modid = Constants.MOD_ID, bus = Bus.MOD)
